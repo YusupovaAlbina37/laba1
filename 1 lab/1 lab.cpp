b102bdf3fdc6e24cf058c6b08623d7fd0e0e85c4
Albina <Us.albina34@gmail.com>
2022-03-25 19:17:36 +0500

    point2

--- 1 lab/1 lab.cpp
+++ 1 lab/1 lab.cpp
@@ -10,7 +10,7 @@ void RenderSceneCB()
 	glClear(GL_COLOR_BUFFER_BIT);////
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);///пеуф
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);////
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);////nnnnjjjj
 	glDrawArrays(GL_POINTS, 0, 1);
 	glDisableVertexAttribArray(0);
 	glutSwapBuffers();
