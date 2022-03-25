ad764546062995b2bcf7c3a60229a2bf4b0deaed
Albina <Us.albina34@gmail.com>
2022-03-25 19:19:09 +0500

    step1,2

--- 1 lab/1 lab.cpp
+++ 1 lab/1 lab.cpp
@@ -7,10 +7,10 @@ GLuint VBO;
 
 void RenderSceneCB()
 {
-	glClear(GL_COLOR_BUFFER_BIT);////
+	glClear(GL_COLOR_BUFFER_BIT);
 	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);///пеуф
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);////nnnnjjjj
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glDrawArrays(GL_POINTS, 0, 1);
 	glDisableVertexAttribArray(0);
 	glutSwapBuffers();
