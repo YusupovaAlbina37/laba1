eed6ec49886fd8fc10fd44543af579705f562c7c
Albina <Us.albina34@gmail.com>
2022-03-25 19:38:45 +0500

    step3

--- 1 lab/1 lab.cpp
+++ 1 lab/1 lab.cpp
@@ -11,7 +11,7 @@ void RenderSceneCB()
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glDrawArrays(GL_POINTS, 0, 1);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 	glDisableVertexAttribArray(0);
 	glutSwapBuffers();
 }
@@ -27,24 +27,23 @@ int main(int argc, char **argv)
 	glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
 	
 
-
 	GLenum res = glewInit();
 	if (res != GLEW_OK)
 	{
 		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
 		return 1;
 	}
-	glm::vec3 Vertices[1];
-	Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
-	
+
+	glm::vec3 Vertices[3];
+	Vertices[0] = glm::vec3(0.0f, 1.0f, 0.0f);
+	Vertices[1] = glm::vec3(-1.0f, 0.0f, 0.0f);
+	Vertices[2] = glm::vec3(0.0f, -1.0f, 0.0f);
+
+
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glDrawArrays(GL_POINTS, 0, 1);
-	glDisableVertexAttribArray(0);
+	
 
 	glutMainLoop();
 }
